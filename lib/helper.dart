15a3df82f1e67bfd867caf30a0d22b04863bf3c0
Isaac Lara <isaac.lara014@gmail.com>
2025-03-05 13:56:33 -0500

    copied helper.dart from in class 8

---
+++ lib/helper.dart
@@ -0,0 +1,115 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "CardOrganizer.db";
+  static const _databaseVersion = 1;
+
+  static const folderTable = 'folders';
+  static const cardTable = 'cards';
+
+  static const columnId = 'id';
+  static const columnName = 'name';
+  static const columnTimestamp = 'timestamp';
+  static const columnSuit = 'suit';
+  static const columnImageUrl = 'imageUrl';
+  static const columnFolderId = 'folderId';
+
+  late Database _db;
+  // this opens the database (and creates it if it doesn't exist)
+  Future<void> init() async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, _databaseName);
+    _db = await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  // SQL code to create the database table
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE $folderTable (
+      $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+      $columnName TEXT NOT NULL,
+      $columnTimestamp TEXT NOT NULL
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE $cardTable (
+      $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+      $columnName TEXT NOT NULL,
+      $columnSuit TEXT NOT NULL,
+      $columnImageUrl TEXT NOT NULL,
+      $columnFolderId INTEGER,
+      FOREIGN KEY ($columnFolderId) REFERENCES $folderTable ($columnId)
+    )
+    ''');
+
+    await _prepopulateCards(db);
+  }
+
+  Future _prepopulateCards(Database db) async {
+    final suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
+    final cardNames = [
+      'Ace',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'Jack',
+      'Queen',
+      'King'
+    ];
+
+    for (var suit in suits) {
+      for (var cardName in cardNames) {
+        await db.insert(cardTable, {
+          'name': '$cardName of $suit',
+          'suit': suit,
+          'imageUrl': 'assets/images/${cardName}_of_${suit}.png',
+        });
+      }
+    }
+  }
+
+  // Helper methods
+  Future<int> insert(String table, Map<String, dynamic> row) async {
+    return await _db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows(String table) async {
+    return await _db.query(table);
+  }
+
+  Future<int> queryRowCount(String table) async {
+    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');
+    return Sqflite.firstIntValue(results) ?? 0;
+  }
+
+  Future<int> update(String table, Map<String, dynamic> row) async {
+    int id = row[columnId];
+    return await _db.update(
+      table,
+      row,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> delete(String table, int id) async {
+    return await _db.delete(
+      table,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'In classwork 9',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Card Organizer'),
     );
   }
 }
@@ -30,40 +31,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late Future<List<Map<String, dynamic>>> _folders;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _folders = _fetchFolders();
+  }
+
+  Future<List<Map<String, dynamic>>> _fetchFolders() async {
+    final db = await DatabaseHelper.instance.database;
+    return db.query('folders');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _folders,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          List<Map<String, dynamic>> folders = snapshot.data!;
+          return ListView.builder(
+            itemCount: folders.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(folders[index]['name']),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CardsScreen(
+                        folderId: folders[index]['id'],
+                        folderName: folders[index]['name'],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
+
+class CardsScreen extends StatefulWidget {}
+
+class _CardsScreenState extends State<CardsScreen> {}
